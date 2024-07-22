c1ba6bbd44de423423157c548bce9236294214cc
Mohd_Hassan_Ashraf <hassanashraf32553@gmail.com>
2024-07-22 20:24:17 +0530

    add javascript

---
+++ section1/loop.js
@@ -0,0 +1,105 @@
+// for (let i = 1; i <= 10; i++) {
+   
+//     num = 19
+//     console.log(`${num} X ${i} = ${num*i}`);
+// }
+
+
+
+
+// for (let i = 50; i <= 100; i++) {
+//     if(i%7 ==0 && i%5==0){
+//         console.log(i);
+//     } 
+// }
+
+
+// let a = 10
+// while(a<20){
+// console.log(a);
+// a++
+// }
+
+
+// prime number 
+
+// let prime = true;
+// const n1 = 18
+// for(let i = 2; i<n1; i++){
+// if (n1%i ===0) {
+//     console.log('not prime');
+//     prime = false
+//     break
+// }
+// }
+// if (prime) {
+//     console.log('prime');
+// }
+
+
+// reverse number 
+
+// let n2 = 235654
+// reverse = 0
+// while(n2>0){
+//     let d = n2%10;
+//     reverse = reverse*10 + d
+//     n2 = parseInt(n2/10)
+// console.log(n2);
+// }
+
+
+// console.log(reverse);
+
+
+
+// fibonaccci series 
+
+// let [a,b] = [0,1]
+// console.log(a);
+// console.log(b);
+
+// for (let i = 0; i < 8; i++) {
+//     let c = a + b;
+//     console.log(c);
+//     [a,b] = [b,c]
+// }
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++ function 
+
+function addNum(a,b){
+    return a+b
+}
+console.log('the sum of a and b is',addNum(12,13));
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+let getFullname = function(first, last){
+    return first + " "+ last
+}
+
+console.log(getFullname('hassan',"ashraf"));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Arrow function 
+
+
+
+let add1 = (x,y) => x+y
+
+let square = (x) => x*x
+
+let multiple = (x) => {
+    console.log(`the value of x is ${x}`);
+    return x*10
+}
+
+console.log(add1(12,13));
+console.log(square(12));
+console.log(multiple(12));
+
---
+++ section1/script.js
@@ -0,0 +1,45 @@
+console.log("hassan ashraf");
+
+
+// num = 10000000
+// console.log(typeof num);
+
+
+// condition 
+n = 54
+
+if (n%2 === 0) {
+    // a = "nice"
+    console.log("even");
+} else {
+    console.log("odd");
+}
+
+// console.log(a);
+
+// question 3
+
+// let a = 12
+// let b = 13 
+// let c = 14 
+
+function greaterNum(a,b,c){
+    if (a>b && a>c) {
+        console.log(`a is greater : ${a}`);
+    } else if (b>a && b>c) {
+        console.log(`b is greater : ${b}`);
+    } else if(c>a && c>b){
+        console.log(`c is greater : ${c}`);
+        
+    }else{
+        console.log('invalid number');
+    }
+}
+
+
+console.log(greaterNum(12,13,14));
+console.log(greaterNum(14,19,15));
+
+
+const num = 36
+
